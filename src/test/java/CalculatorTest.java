1d042b27b6249fc35f183147b9d2b8c1b6ecc32e
Victoria Holland <JavaDevVictoria@users.noreply.github.com>
2024-04-22 15:59:14 +0000

    Added unit tests and coded methods for calcualtor

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,9 +9,35 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         int expected = 3;
-
         int actual = calculator.add(1, 2);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void testSubtract() {
+        int expected = 2;
+        int actual = calculator.subtract(3, 1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMultiply() {
+        int expected = 6;
+        int actual = calculator.multiply(2, 3);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDivide() {
+        int expected = 3;
+        int actual = calculator.divide(6, 2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDivideWhenResultIsDecimal() {
+        double expected = 1.0;
+        int actual = calculator.divide(6, 5);
         assertEquals(expected, actual);
     }
 
