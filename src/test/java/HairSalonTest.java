18e53d239a89ad57ebdc499d17f3bec6afb6e32b
Victoria Holland <JavaDevVictoria@users.noreply.github.com>
2024-04-23 08:44:42 +0000

    Initial commit

--- src/test/java/HairSalonTest.java
+++ src/test/java/HairSalonTest.java
@@ -1,3 +1,8 @@
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class HairSalonTest {
@@ -6,22 +11,30 @@ class HairSalonTest {
 
     @Test
     public void testGetOpeningDays() {
-
+        String[] expected = { "Monday", "Tuesday" };
+        String[] actual = hairSalon.getOpeningDays();
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     public void testSalonIsOpenOnMonday() {
-
+        boolean actual = hairSalon.isOpen("Monday");
+        assertTrue(actual);
     }
 
     @Test
     public void testSalonIsClosedOnWednesday() {
+        boolean actual = hairSalon.isOpen("Wednesday");
+        assertFalse(actual);
 
     }
 
     @Test
     public void testExceptionIsThrownIfWeekdayDoesNotEndInDay() {
 
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> hairSalon.isOpen(1, 0));
+
+        String expected = "Only weekdays are allowed.";
     }
 
 }
