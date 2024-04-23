5d54e46b696ef9a4384f98de71d946a671e4827a
Victoria Holland <JavaDevVictoria@users.noreply.github.com>
2024-04-23 11:40:53 +0000

    Written unit tests

--- src/main/java/DateChecker.java
+++ src/main/java/DateChecker.java
@@ -17,11 +17,15 @@ public class DateChecker {
         DECEMBER
     }
 
+    private Month[] monthWith31Days = { Month.JANUARY, Month.MARCH, Month.MAY, Month.JULY, Month.AUGUST, Month.OCTOBER,
+            Month.DECEMBER };
+
     public boolean has31Days(Month month) {
-        return false;
+        if (Arrays.asList(monthWith31Days).contains(month)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
-    
-
 
 }
--- src/test/java/DateCheckerTest.java
+++ src/test/java/DateCheckerTest.java
@@ -1,18 +1,22 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DateCheckerTest {
 
-
-    @Test
-    public void testHas31Days() {
-
+    @ParameterizedTest
+    @EnumSource(value = DateChecker.Month.class, names = { "JANUARY", "MARCH", "MAY", "JULY", "AUGUST", "OCTOBER",
+            "DECEMBER" })
+    public void testHas31Days(DateChecker.Month month) {
+        assertTrue(DateChecker.has31Days(month));
     }
 
-    @Test
-    public void testDoesNotHave31Days() {
-
+    @ParameterizedTest
+    @EnumSource(value = DateChecker.Month.class, names = { "FEBRUARY", "APRIL", "JUNE", "SEPTEMBER", "NOVEMBER" })
+    public void testDoesNotHave31Days(DateChecker.Month month) {
+        assertFalse(DateChecker.has31Days(month));
     }
 
 }
